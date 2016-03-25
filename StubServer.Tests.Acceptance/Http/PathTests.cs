9d38201a278e2df4e130ad6324b2362b5748a31c
Ahmed Agabani <agabani@live.com>
2016-03-25 17:48:58 +0000

    Added path tests

---
+++ StubServer.Tests.Acceptance/Http/PathTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using NUnit.Framework;
+
+namespace StubServer.Tests.Acceptance.Http
+{
+    internal class PathTests : HttpStubServerTests
+    {
+        [Test]
+        public void Should_return_configured_response_if_has_been_setup_and_triggered()
+        {
+            var random = Guid.NewGuid().ToString();
+
+            HttpStubServer
+                .Setup(message => message.RequestUri.PathAndQuery.Equals("/respected/path/" + random))
+                .Returns(() => new HttpResponseMessage(HttpStatusCode.OK));
+
+            HttpResponseMessage = HttpClient
+                .SendAsync(HttpRequestMessage = new HttpRequestMessage(HttpMethod.Get, "respected/path/" + random))
+                .GetAwaiter().GetResult();
+
+            Assert.That(HttpResponseMessage.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+
+        [Test]
+        public void Should_return_Not_Found_response_if_has_not_been_setup()
+        {
+            var random = Guid.NewGuid().ToString();
+
+            HttpResponseMessage = HttpClient
+                .SendAsync(HttpRequestMessage = new HttpRequestMessage(HttpMethod.Get, "respected/path/" + random))
+                .GetAwaiter().GetResult();
+
+            Assert.That(HttpResponseMessage.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+        }
+
+        [Test]
+        public void Should_return_Not_Found_response_if_has_been_setup_and_not_triggered()
+        {
+            var random = Guid.NewGuid().ToString();
+
+            HttpStubServer
+                .Setup(message => message.RequestUri.PathAndQuery.Equals("/respected/path/" + random))
+                .Returns(() => new HttpResponseMessage(HttpStatusCode.OK));
+
+            HttpResponseMessage = HttpClient
+                .SendAsync(HttpRequestMessage = new HttpRequestMessage(HttpMethod.Get, "unrespected/path/" + random))
+                .GetAwaiter().GetResult();
+
+            Assert.That(HttpResponseMessage.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+        }
+    }
+}
