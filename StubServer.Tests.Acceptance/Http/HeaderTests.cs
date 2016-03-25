5a6e0d6d4667beffef0ed3f77ea0e8ac1dbca8d6
Ahmed Agabani <agabani@live.com>
2016-03-25 17:57:07 +0000

    Added header tests

---
+++ StubServer.Tests.Acceptance/Http/HeaderTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using NUnit.Framework;
+
+namespace StubServer.Tests.Acceptance.Http
+{
+    internal class HeaderTests : HttpStubServerTests
+    {
+        [Test]
+        public void Should_read_headers()
+        {
+            var token = Guid.NewGuid().ToString();
+
+            HttpStubServer
+                .Setup(message => message.Headers.Authorization.Scheme == "Basic" &&
+                                  message.Headers.Authorization.Parameter == token)
+                .Returns(() => new HttpResponseMessage(HttpStatusCode.Forbidden));
+
+            HttpResponseMessage = HttpClient
+                .SendAsync(HttpRequestMessage = new HttpRequestMessage(HttpMethod.Post, "/")
+                {
+                    Headers = {Authorization = new AuthenticationHeaderValue("Basic", token)}
+                })
+                .GetAwaiter().GetResult();
+
+            Assert.That(HttpResponseMessage.StatusCode, Is.EqualTo(HttpStatusCode.Forbidden));
+        }
+    }
+}
