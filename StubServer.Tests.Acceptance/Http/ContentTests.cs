4d2609e64cdad805c02742187731dee039853313
Ahmed Agabani <agabani@live.com>
2016-03-25 17:38:04 +0000

    Added content tests

---
+++ StubServer.Tests.Acceptance/Http/ContentTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using NUnit.Framework;
+
+namespace StubServer.Tests.Acceptance.Http
+{
+    internal class ContentTests : HttpStubServerTests
+    {
+        [Test]
+        public void Should_return_content()
+        {
+            var randomClientContent = Guid.NewGuid().ToString();
+            var randomServerContent = Guid.NewGuid().ToString();
+
+            HttpStubServer
+                .Setup(message => message.Content.ReadAsStringAsync().GetAwaiter().GetResult() == randomClientContent)
+                .Returns(() => new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(randomServerContent)
+                });
+
+            HttpResponseMessage = HttpClient
+                .SendAsync(HttpRequestMessage = new HttpRequestMessage(HttpMethod.Post, "/")
+                {
+                    Content = new StringContent(randomClientContent)
+                })
+                .GetAwaiter().GetResult();
+
+            Assert.That(HttpResponseMessage.Content.ReadAsStringAsync().GetAwaiter().GetResult(),
+                Is.EqualTo(randomServerContent));
+        }
+    }
+}
