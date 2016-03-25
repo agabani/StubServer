b5a766df00bf948c195866b72229f2b193f9840d
Ahmed Agabani <agabani@live.com>
2016-03-25 20:02:12 +0000

    Added nuspec

--- StubServer/Properties/AssemblyInfo.cs
+++ StubServer/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("StubServer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Quickly stub out 3rd party servers to be used against your automated development test cycle. Designed to feel familiar to use, without having restrictions forced upon you.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("StubServer")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
