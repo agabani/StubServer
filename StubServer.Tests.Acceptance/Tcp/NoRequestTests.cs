0d33b20b3843c2929953f0b96f8b7792dd1e6a1f
Ahmed Agabani <agabani@live.com>
2016-03-31 23:23:29 +0100

    TCP - No request tests

---
+++ StubServer.Tests.Acceptance/Tcp/NoRequestTests.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Text;
+using NUnit.Framework;
+
+namespace StubServer.Tests.Acceptance.Tcp
+{
+    internal class NoRequestTests : TcpStubServerTests
+    {
+        [Test]
+        public void Should_not_response_to_no_request()
+        {
+            // Arrange
+            TcpStubServer
+                .Setup(message => true)
+                .Returns(() => Encoding.UTF8.GetBytes("Setup"));
+
+            NetworkStream.Write(new byte[] { });
+
+            // Act
+            TestDelegate testDelegate = () => NetworkStream.Read();
+
+            // Assert
+            Assert.Throws<IOException>(testDelegate);
+        }
+    }
+}
