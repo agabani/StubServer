98891d292a7186bbd48dff80909c758abd491cde
Ahmed Agabani <agabani@live.com>
2016-03-31 01:30:46 +0100

    UDP - Last return repeats

---
+++ StubServer.Tests.Acceptance/Udp/MultipleReturnTests.cs
@@ -0,0 +1,80 @@
+﻿using System.Text;
+using NUnit.Framework;
+
+namespace StubServer.Tests.Acceptance.Udp
+{
+    internal class MultipleReturnTests : UdpStubServerTests
+    {
+        [Test]
+        public void Should_return_response()
+        {
+            // Arrange
+            UdpStubServer
+                .Setup(o => Encoding.UTF8.GetString(o).Equals("Hello, World!"))
+                .Returns(() => Encoding.UTF8.GetBytes("John Smith"))
+                .Returns(() => Encoding.UTF8.GetBytes("James Bond"))
+                .Returns(() => Encoding.UTF8.GetBytes("Bob Marley"));
+
+            // Act and Assert
+            UdpClient
+                .Send(Encoding.UTF8.GetBytes("Hello, World!"));
+
+            var response = Encoding.UTF8.GetString(UdpClient.Receive());
+
+            Assert.That(response, Is.EqualTo("John Smith"));
+
+            UdpClient
+                .Send(Encoding.UTF8.GetBytes("Hello, World!"));
+
+            response = Encoding.UTF8.GetString(UdpClient.Receive());
+
+            Assert.That(response, Is.EqualTo("James Bond"));
+
+            UdpClient
+                .Send(Encoding.UTF8.GetBytes("Hello, World!"));
+
+            response = Encoding.UTF8.GetString(UdpClient.Receive());
+
+            Assert.That(response, Is.EqualTo("Bob Marley"));
+        }
+
+        [Test]
+        public void Should_return_response_then_repeat_final_response()
+        {
+            // Arrange
+            UdpStubServer
+                .Setup(o => Encoding.UTF8.GetString(o).Equals("Hello, World!"))
+                .Returns(() => Encoding.UTF8.GetBytes("John Smith"))
+                .Returns(() => Encoding.UTF8.GetBytes("James Bond"));
+
+            // Act and Assert
+            UdpClient
+                .Send(Encoding.UTF8.GetBytes("Hello, World!"));
+
+            var response = Encoding.UTF8.GetString(UdpClient.Receive());
+
+            Assert.That(response, Is.EqualTo("John Smith"));
+
+            UdpClient
+                .Send(Encoding.UTF8.GetBytes("Hello, World!"));
+
+            response = Encoding.UTF8.GetString(UdpClient.Receive());
+
+            Assert.That(response, Is.EqualTo("James Bond"));
+
+            UdpClient
+                .Send(Encoding.UTF8.GetBytes("Hello, World!"));
+
+            response = Encoding.UTF8.GetString(UdpClient.Receive());
+
+            Assert.That(response, Is.EqualTo("James Bond"));
+
+            UdpClient
+                .Send(Encoding.UTF8.GetBytes("Hello, World!"));
+
+            response = Encoding.UTF8.GetString(UdpClient.Receive());
+
+            Assert.That(response, Is.EqualTo("James Bond"));
+        }
+    }
+}
--- StubServer/Udp/UdpSetup.cs
+++ StubServer/Udp/UdpSetup.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,6 +10,10 @@ namespace StubServer.Udp
     public class UdpSetup : ISetup<byte[]>
     {
         private readonly Func<byte[], bool> _expression;
+
+        private readonly Queue<Func<CancellationToken, Task<byte[]>>> _responses =
+            new Queue<Func<CancellationToken, Task<byte[]>>>();
+
         private Func<CancellationToken, Task<byte[]>> _response;
 
         public UdpSetup(Expression<Func<byte[], bool>> expression)
@@ -17,30 +23,29 @@ namespace StubServer.Udp
 
         public ISetup<byte[]> Returns(Func<byte[]> response)
         {
-            _response = cancellationToken => Task.FromResult(response());
+            _responses.Enqueue(cancellationToken => Task.FromResult(response()));
             return this;
         }
 
         public ISetup<byte[]> Returns(Func<Task<byte[]>> response)
         {
-            _response = cancellationToken => response();
+            _responses.Enqueue(cancellationToken => response());
             return this;
         }
 
         public ISetup<byte[]> Returns(Func<CancellationToken, Task<byte[]>> response)
         {
-            _response = response;
+            _responses.Enqueue(response);
             return this;
         }
 
         internal Task<byte[]> Result(byte[] request, CancellationToken cancellationToken)
         {
-            if (_expression(request))
-            {
-                return _response(cancellationToken);
-            }
-
-            return Task.FromResult<byte[]>(null);
+            return _expression(request)
+                ? _responses.Any()
+                    ? (_response = _responses.Dequeue())(cancellationToken)
+                    : _response(cancellationToken)
+                : Task.FromResult<byte[]>(null);
         }
     }
 }
