586e9c594682a91a0a7d3d3cdfb0293250ddccaa
Ahmed Agabani <agabani@live.com>
2016-04-02 16:09:14 +0100

    UDP - Concurrent tests

---
+++ StubServer.Tests.Acceptance/Udp/ConcurrentClientsTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using NUnit.Framework;
+
+namespace StubServer.Tests.Acceptance.Udp
+{
+    internal class ConcurrentClientsTests : UdpStubServerTests
+    {
+        [Test]
+        public void Should_return_chained_request_over_multiple_clients()
+        {
+            // Arrange
+            var udpStubServer = NewStubServer();
+
+            udpStubServer
+                .Setup(o => Encoding.UTF8.GetString(o).Equals("Hello, World!"))
+                .Returns(() => Encoding.UTF8.GetBytes("John Smith"))
+                .Returns(() => Encoding.UTF8.GetBytes("James Bond"))
+                .Returns(() => Encoding.UTF8.GetBytes("Bob Marley"));
+
+            var udpClient1 = NewUdpClient();
+            var udpClient2 = NewUdpClient();
+
+            // Act & Assert
+            udpClient1.Send(Encoding.UTF8.GetBytes("Hello, World!"));
+            Assert.That(Encoding.UTF8.GetString(udpClient1.Receive()), Is.EqualTo("John Smith"));
+
+            // Act & Assert
+            udpClient2.Send(Encoding.UTF8.GetBytes("Hello, World!"));
+            Assert.That(Encoding.UTF8.GetString(udpClient2.Receive()), Is.EqualTo("James Bond"));
+
+            // Act & Assert
+            udpClient1.Send(Encoding.UTF8.GetBytes("Hello, World!"));
+            Assert.That(Encoding.UTF8.GetString(udpClient1.Receive()), Is.EqualTo("Bob Marley"));
+
+            // Act & Assert
+            udpClient2.Send(Encoding.UTF8.GetBytes("Hello, World!"));
+            Assert.That(Encoding.UTF8.GetString(udpClient2.Receive()), Is.EqualTo("Bob Marley"));
+
+            // Cleanup
+            Cleanup(udpClient1);
+            Cleanup(udpClient2);
+            Cleanup(udpStubServer);
+        }
+    }
+}
