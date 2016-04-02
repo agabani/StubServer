ccc8e8eff74f5572fd41d8acef62545ea7c30c25
Ahmed Agabani <agabani@live.com>
2016-04-02 15:59:47 +0100

    TCP - Concurrent tests refactor

--- StubServer.Tests.Acceptance/Tcp/ConcurrentClientsTests.cs
+++ StubServer.Tests.Acceptance/Tcp/ConcurrentClientsTests.cs
@@ -1,19 +1,15 @@
-﻿using System;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
+﻿using System.Text;
 using NUnit.Framework;
-using StubServer.Tcp;
 
 namespace StubServer.Tests.Acceptance.Tcp
 {
-    internal class ConcurrentClientsTests
+    internal class ConcurrentClientsTests : TcpStubServerTests
     {
         [Test]
         public void Should_return_chained_request_over_multiple_clients()
         {
             // Arrange
-            ITcpStubServer tcpStubServer = new TcpStubServer(IPAddress.Loopback, 5053);
+            var tcpStubServer = NewStubServer();
 
             tcpStubServer
                 .Setup(o => Encoding.UTF8.GetString(o).Equals("Hello, World!"))
@@ -21,47 +17,34 @@ namespace StubServer.Tests.Acceptance.Tcp
                 .Returns(() => Encoding.UTF8.GetBytes("James Bond"))
                 .Returns(() => Encoding.UTF8.GetBytes("Bob Marley"));
 
-            var tcpClient1 = new TcpClient
-            {
-                Client = {ReceiveTimeout = (int) TimeSpan.FromSeconds(1).TotalMilliseconds}
-            };
-
-            tcpClient1.Connect(new IPEndPoint(IPAddress.Loopback, 5053));
+            var tcpClient1 = NewTcpClient();
             var networkStream1 = tcpClient1.GetStream();
 
-            var tcpClient2 = new TcpClient
-            {
-                Client = { ReceiveTimeout = (int)TimeSpan.FromSeconds(1).TotalMilliseconds }
-            };
-
-            tcpClient2.Connect(new IPEndPoint(IPAddress.Loopback, 5053));
+            var tcpClient2 = NewTcpClient();
             var networkStream2 = tcpClient2.GetStream();
 
-            // Act and Assert
+            // Act & Assert
             networkStream1.Write(Encoding.UTF8.GetBytes("Hello, World!"));
-            var response = Encoding.UTF8.GetString(networkStream1.Read());
-            Assert.That(response, Is.EqualTo("John Smith"));
+            Assert.That(Encoding.UTF8.GetString(networkStream1.Read()), Is.EqualTo("John Smith"));
 
-            networkStream1.Write(Encoding.UTF8.GetBytes("Hello, World!"));
-            response = Encoding.UTF8.GetString(networkStream1.Read());
-            Assert.That(response, Is.EqualTo("James Bond"));
+            // Act & Assert
+            networkStream2.Write(Encoding.UTF8.GetBytes("Hello, World!"));
+            Assert.That(Encoding.UTF8.GetString(networkStream2.Read()), Is.EqualTo("James Bond"));
 
+            // Act & Assert
             networkStream1.Write(Encoding.UTF8.GetBytes("Hello, World!"));
-            response = Encoding.UTF8.GetString(networkStream1.Read());
-            Assert.That(response, Is.EqualTo("Bob Marley"));
+            Assert.That(Encoding.UTF8.GetString(networkStream1.Read()), Is.EqualTo("Bob Marley"));
 
-            networkStream1.Write(Encoding.UTF8.GetBytes("Hello, World!"));
-            response = Encoding.UTF8.GetString(networkStream1.Read());
-            Assert.That(response, Is.EqualTo("Bob Marley"));
+            // Act & Assert
+            networkStream2.Write(Encoding.UTF8.GetBytes("Hello, World!"));
+            Assert.That(Encoding.UTF8.GetString(networkStream2.Read()), Is.EqualTo("Bob Marley"));
 
             // Cleanup
-            networkStream1.Dispose();
-            tcpClient1.Close();
-
-            networkStream2.Dispose();
-            tcpClient2.Close();
-
-            tcpStubServer.Dispose();
+            Cleanup(networkStream1);
+            Cleanup(tcpClient1);
+            Cleanup(networkStream2);
+            Cleanup(tcpClient2);
+            Cleanup(tcpStubServer);
         }
     }
 }
