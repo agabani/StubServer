771cbee5d9b4ca020f7d77f738be68bc806379e4
Ahmed Agabani <agabani@live.com>
2016-04-04 03:07:31 +0100

    Revision 0.1.6.0

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 > Can be as simple as you want... Or as complex...
 
-[![NuGet downloads](https://img.shields.io/badge/nuget-v0.1.5-blue.svg)](https://www.nuget.org/packages/StubServer)
+[![NuGet downloads](https://img.shields.io/badge/nuget-v0.1.6-blue.svg)](https://www.nuget.org/packages/StubServer)
 
 Checkout the [Wiki](https://github.com/agabani/StubServer/wiki) for more examples!
 
@@ -89,4 +89,61 @@ Assert.That(Encoding.UTF8.GetString(receive), Is.EqualTo("Hello, World!"));
 // Cleanup
 udpStubServer.Dispose();
 udpClient.Close();
+```
+
+### Example SMTP StubServer
+```csharp
+// Arrange
+ISmtpStubServer smtpStubServer = new SmtpStubServer(IPAddress.Loopback, 5000);
+
+smtpStubServer
+	.Setup(bytes => Encoding.UTF8.GetString(bytes).StartsWith("EHLO"))
+	.Returns(() => Encoding.UTF8.GetBytes(
+		"250-smtp.example.com Hello www.example.org [123.0.0.321]\r\n" +
+		"250-SIZE 14680064\r\n" +
+		"250-PIPELINING\r\n" +
+		"250 HELP\r\n"));
+
+smtpStubServer
+	.Setup(bytes => Encoding.UTF8.GetString(bytes).Equals("MAIL FROM:<jane@contoso.com>\r\n"))
+	.Returns(() => Encoding.UTF8.GetBytes("250 Ok\r\n"));
+
+smtpStubServer
+	.Setup(bytes => Encoding.UTF8.GetString(bytes).Equals("RCPT TO:<ben@contoso.com>\r\n"))
+	.Returns(() => Encoding.UTF8.GetBytes("250 Ok\r\n"));
+
+smtpStubServer
+	.Setup(bytes => Encoding.UTF8.GetString(bytes).Equals("DATA\r\n"))
+	.Returns(() => Encoding.UTF8.GetBytes("354 End data with <CR><LF>.<CR><LF>\r\n"));
+
+smtpStubServer
+	.Setup(bytes => Encoding.UTF8.GetString(bytes).Contains("\r\n.\r\n"))
+	.Returns(() => Encoding.UTF8.GetBytes("250 Ok: queued as 12345\r\n"));
+
+smtpStubServer
+	.Setup(bytes => Encoding.UTF8.GetString(bytes).Equals("QUIT\r\n"))
+	.Returns(() => Encoding.UTF8.GetBytes("221 Bye\r\n"));
+
+var smtpClient = new SmtpClient("127.0.0.1", 5000);
+
+var message = new MailMessage(
+	new MailAddress("jane@contoso.com", "Jane Clayton"),
+	new MailAddress("ben@contoso.com"))
+{
+	Body = "This is a test e-mail message sent by an application.",
+	BodyEncoding = Encoding.UTF8,
+	Subject = "test message 1",
+	SubjectEncoding = Encoding.UTF8
+};
+
+// Act
+TestDelegate testDelegate = () => smtpClient.Send(message);
+
+// Assert
+Assert.DoesNotThrow(testDelegate);
+
+// Cleanup
+message.Dispose();
+smtpClient.Dispose();
+smtpStubServer.Dispose();
 ```
---
+++ StubServer.Tests.Acceptance/Smtp/SampleTests.cs
@@ -0,0 +1,69 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Text;
+using NUnit.Framework;
+using StubServer.Smtp;
+
+namespace StubServer.Tests.Acceptance.Smtp
+{
+    internal class SampleTests
+    {
+        [Test]
+        public void DemoTests()
+        {
+            // Arrange
+            ISmtpStubServer smtpStubServer = new SmtpStubServer(IPAddress.Loopback, 5000);
+
+            smtpStubServer
+                .Setup(bytes => Encoding.UTF8.GetString(bytes).StartsWith("EHLO"))
+                .Returns(() => Encoding.UTF8.GetBytes(
+                    "250-smtp.example.com Hello www.example.org [123.0.0.321]\r\n" +
+                    "250-SIZE 14680064\r\n" +
+                    "250-PIPELINING\r\n" +
+                    "250 HELP\r\n"));
+
+            smtpStubServer
+                .Setup(bytes => Encoding.UTF8.GetString(bytes).Equals("MAIL FROM:<jane@contoso.com>\r\n"))
+                .Returns(() => Encoding.UTF8.GetBytes("250 Ok\r\n"));
+
+            smtpStubServer
+                .Setup(bytes => Encoding.UTF8.GetString(bytes).Equals("RCPT TO:<ben@contoso.com>\r\n"))
+                .Returns(() => Encoding.UTF8.GetBytes("250 Ok\r\n"));
+
+            smtpStubServer
+                .Setup(bytes => Encoding.UTF8.GetString(bytes).Equals("DATA\r\n"))
+                .Returns(() => Encoding.UTF8.GetBytes("354 End data with <CR><LF>.<CR><LF>\r\n"));
+
+            smtpStubServer
+                .Setup(bytes => Encoding.UTF8.GetString(bytes).Contains("\r\n.\r\n"))
+                .Returns(() => Encoding.UTF8.GetBytes("250 Ok: queued as 12345\r\n"));
+
+            smtpStubServer
+                .Setup(bytes => Encoding.UTF8.GetString(bytes).Equals("QUIT\r\n"))
+                .Returns(() => Encoding.UTF8.GetBytes("221 Bye\r\n"));
+
+            var smtpClient = new SmtpClient("127.0.0.1", 5000);
+
+            var message = new MailMessage(
+                new MailAddress("jane@contoso.com", "Jane Clayton"),
+                new MailAddress("ben@contoso.com"))
+            {
+                Body = "This is a test e-mail message sent by an application.",
+                BodyEncoding = Encoding.UTF8,
+                Subject = "test message 1",
+                SubjectEncoding = Encoding.UTF8
+            };
+
+            // Act
+            TestDelegate testDelegate = () => smtpClient.Send(message);
+
+            // Assert
+            Assert.DoesNotThrow(testDelegate);
+
+            // Cleanup
+            message.Dispose();
+            smtpClient.Dispose();
+            smtpStubServer.Dispose();
+        }
+    }
+}
